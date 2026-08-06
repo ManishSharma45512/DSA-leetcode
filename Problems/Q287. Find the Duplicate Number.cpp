f19f830d5d5242542ef6592194f0499c7f38565c
Manish <manishsharma.45512@gmail.com>
2026-08-06 22:20:56 +0530

    Create Q287. Find the Duplicate Number.cpp

---
+++ Problems/Q287. Find the Duplicate Number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+        }
+
+        for(const auto& p : freq){
+            if(p.second>1){
+                return p.first;
+            }
+        }
+
+        return -1;
+    }
+};
