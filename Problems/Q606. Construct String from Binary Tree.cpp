de75aa086630083dfac2346707041c92a1661f07
Manish <manishsharma.45512@gmail.com>
2026-09-02 00:08:28 +0530

    Create Q606. Construct String from Binary Tree.cpp

---
+++ Problems/Q606. Construct String from Binary Tree.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    string helper(TreeNode* root) {
+        if (root == nullptr) return "";
+
+        string res = to_string(root->val);
+
+        if (root->left == nullptr && root->right == nullptr) {//Leaf Node
+            return res;
+        }
+
+        res += "(" + helper(root->left) + ")";
+
+        if (root->right != nullptr) {
+            res += "(" + helper(root->right) + ")";
+        }
+
+        return res;
+    }
+
+public:
+    string tree2str(TreeNode* root) {
+        return helper(root);
+    }
+};
