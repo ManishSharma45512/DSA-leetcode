8ff9582ad807a9c50b15e6231ce0c3cc2ac9d6ca
Manish <manishsharma.45512@gmail.com>
2026-09-06 19:37:40 +0530

    Create Q115. Distinct Subsequences.cpp

---
+++ Problems/Q115. Distinct Subsequences.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.length();
+        int n = t.length();
+        
+        if (m < n) return 0;
+        
+        // Simple int array
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        
+        for (int i = 1; i <= m; ++i) {
+            for (int j = n; j >= 1; --j) {
+                if (s[i - 1] == t[j - 1]) {
+                    if (dp[j] <= INT_MAX - dp[j - 1]) {
+                        dp[j] += dp[j - 1];
+                    } else {
+                        dp[j] = INT_MAX;
+                    }
+                }
+            }
+        }
+        
+        return dp[n];
+    }
+};
